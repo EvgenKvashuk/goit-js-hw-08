aab61bd6196e6e38d288ae652d0eda7795f15259
EvgenKvashuk <105291436+EvgenKvashuk@users.noreply.github.com>
2023-04-13 23:12:31 +0300

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,8 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items';
 
-// const galleryMarkup = createGalleryCardMarkup(galleryItems);
-
+const galleryMarkup = createGalleryCardMarkup(galleryItems);
 function createGalleryCardMarkup(galleryItem) {
   return galleryItems
     .map(({ preview, original, description }) => {
@@ -11,43 +10,38 @@ function createGalleryCardMarkup(galleryItem) {
                 <a class="gallery__link" href="${original}">
                     <img
                         class="gallery__image"
-                        src="${preview}"
-                        data-source="${original}"
+                        src="${original}"
                         alt="${description}"
                     />
                 </a>
             </div>`;
     })
-    .join("");
+    .join('');
 }
 
-const galleryEl = document.querySelector(".gallery");
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('afterbegin', galleryMarkup);
 
-galleryEl.insertAdjacentHTML("afterbegin", galleryMarkup);
 
-galleryEl.addEventListener("click", onGalleryClick);
-
+galleryEl.addEventListener('click', onGalleryClick);
 function onGalleryClick(event) {
   event.preventDefault();
-  if (!event.target.classList.contains("gallery__image")) {
-    return;
-  }
-
-  const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}">
-`);
-
-  console.log(instance);
-
-  instance.show();
-
-  if (instance.show()) {
-    window.addEventListener("keydown", onKeyDown);
-
-    function onKeyDown(event) {
-      if (event.key === "Escape") {
-        instance.close();
-      }
-    }
-  }
+  `<div class="gallery">
+    <a href="${event.target.getAttribute(
+      'href'
+    )}"><img src="${event.target.getAttribute(
+    'src'
+  )}" alt="${event.target.getAttribute('alt')}" /></a>
+    <a href="${event.target.getAttribute(
+      'href'
+    )}"><img src="${event.target.getAttribute(
+    'src'
+  )}" alt="${event.target.getAttribute('alt')}"/></a>
+</div>
+    `;
 }
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  showCounter: false,
+});
